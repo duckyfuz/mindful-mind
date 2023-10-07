1fa0b3bb7d9274f5d798972345d307106c1e640a
King Nerdy <ernest.tanhk@gmail.com>
2023-10-07 15:51:51 +0800

    whisper

--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+
+const apiKey = import.meta.env.VITE_OPENAI_KEY;
+const model = "whisper-1";
+const language = "en";
 
 const Record = () => {
-  return <div>Record</div>;
+  const inputRef = useRef();
+  const [file, setFile] = useState();
+  const [response, setResponse] = useState(null);
+
+  const onChangeFile = () => {
+    setFile(inputRef.current.files[0]);
+  };
+
+  useEffect(() => {
+    const fetchAudioFile = async () => {
+      if (!file) {
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("model", model);
+      formData.append("file", file);
+      formData.append("language", language);
+
+      axios
+        .post("https://api.openai.com/v1/audio/transcriptions", formData, {
+          headers: {
+            "Content-Type": "multi-part/form-data",
+            Authorization: `Bearer ${apiKey}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setResponse(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchAudioFile();
+  }, [file]);
+
+  return (
+    <div>
+      Whisper
+      <input type="file" ref={inputRef} accept=".mp3" onChange={onChangeFile} />
+      {response && <div>{JSON.stringify(response, null, 2)}</div>}
+    </div>
+  );
 };
 
 export default Record;
