d14724040fcb4a14b695e92cc3af62ab5af9cc60
King Nerdy <ernest.tanhk@gmail.com>
2023-10-07 18:54:49 +0800

    resources

---
+++ src/Components/resources.jsx
@@ -0,0 +1,12 @@
+const resources = {
+  sad: ["Resource 1", "Resource2"],
+  happy: ["Resource 1", "Resource2"],
+  scared: ["Resource 1", "Resource2"],
+  loved: ["Resource 1", "Resource2"],
+  angry: ["Resource 1", "Resource2"],
+  confident: ["Resource 1", "Resource2"],
+  embarassed: ["Resource 1", "Resource2"],
+  playful: ["Resource 1", "Resource2"],
+};
+
+export default resources;
