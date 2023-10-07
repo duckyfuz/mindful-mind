ea222f5b3800de9bd4af89d3ab35f0c2b31e5713
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-10-07 11:06:23 +0800

    Add examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Stack, Text } from "@chakra-ui/react";
 
 import { Button } from "@chakra-ui/react";
+import { Card, CardHeader, CardBody, CardFooter } from "@chakra-ui/react";
+
+import { entries } from "./tmp/example";
 
 function App() {
   return (
     <ChakraProvider>
-      <Button colorScheme="teal" size="lg">
-        Button
-      </Button>
+      <Stack>
+        {Object.keys(entries).map((entryKey, index) => (
+          <Card key={index}>
+            <CardBody>
+              <Text>{entries[entryKey]}</Text>
+            </CardBody>
+          </Card>
+        ))}
+        <Button colorScheme="teal" size="lg">
+          Button
+        </Button>
+      </Stack>
     </ChakraProvider>
   );
 }
---
+++ src/tmp/example.js
@@ -0,0 +1,7 @@
+export const entries = {
+  // Entry 1
+  "2023-10-01T08:00:00": "Feeling refreshed and motivated today.",
+
+  // Entry 2
+  "2023-10-02T12:30:00": "A bit stressed due to work deadlines.",
+};
