ae0a9280d28c9d53cfead1f772c4b84b6bdb1dd2
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-10-07 12:33:37 +0800

    Add OpenAI

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 //components
-import Home from "./App.jsx";
+import App from "./App.jsx";
 import ChatGPT from "./chat/ChatGPT.jsx";
 import Record from "./pages/Record.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <App />,
   },
   {
     path: "/record",
--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,37 +1,7 @@
-import { useState } from "react";
-import { Textarea, Button, Box, VStack } from "@chakra-ui/react";
-import { useSpeechRecognition } from "react-speech-kit";
+import React from "react";
 
 const Record = () => {
-  const [value, setValue] = useState("");
-  const { listen, stop } = useSpeechRecognition({
-    onResult: (result) => {
-      console.log("hello");
-      setValue(result);
-    },
-  });
-
-  return (
-    <Box display="flex" backgroundColor={"red"}>
-      <VStack>
-        <Textarea
-          resize={"vertical"}
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
-        />
-        <Button onMouseDown={listen} onMouseUp={stop}>
-          🎤
-        </Button>
-        <Button
-          onClick={() => {
-            setValue("he");
-          }}
-        >
-          Add entry
-        </Button>
-      </VStack>
-    </Box>
-  );
+  return <div>Record</div>;
 };
 
 export default Record;
