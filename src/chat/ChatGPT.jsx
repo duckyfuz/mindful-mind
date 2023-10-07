f0924a72131f553c472a4a5023c6cc4281aaf239
King Nerdy <ernest.tanhk@gmail.com>
2023-10-07 12:06:46 +0800

    LUNCHTIME and we have 0 progress

--- src/chat/ChatGPT.jsx
+++ src/chat/ChatGPT.jsx
@@ -1,11 +1,46 @@
 import React from "react";
-import { Button, ButtonGroup, Textarea } from "@chakra-ui/react";
+import { Button, ButtonGroup, Textarea, Stack } from "@chakra-ui/react";
 import { useState } from "react";
+import axios from "axios";
 
 const ChatGPT = () => {
+  const [inputText, setInputText] = useState("");
+  const [response, setResponse] = useState("");
+
+  const apiKey = "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"; // Replace with your API key
+  const apiUrl = "	https://api.openai.com/v1/chat/completions"; // Change the engine if needed
+
+  const generateResponse = async () => {
+    try {
+      const requestBody = {
+        prompt: inputText,
+        max_tokens: 500, // Adjust as needed
+      };
+
+      const response = await axios.post(apiUrl, requestBody, {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          "Content-Type": "application/json",
+        },
+      });
+
+      setResponse(response.data.choices[0].text);
+    } catch (error) {
+      console.error("Error generating response:", error);
+    }
+  };
+
   return (
     <div>
-      <Button colorScheme="blue">Send to ChatGPT</Button>
+      <Stack direction="column">
+        <Textarea
+          placeholder="Here is a sample placeholder"
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <Button colorScheme="blue" onClick={generateResponse}>
+          Send to ChatGPT
+        </Button>
+      </Stack>
     </div>
   );
 };
