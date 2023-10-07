136c6da0c491a44acb07365e7213aed6b728d87f
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-10-07 12:07:42 +0800

    Add audio

--- src/pages/Record.jsx
+++ src/pages/Record.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState } from "react";
+import { Textarea, Button, Box, VStack } from "@chakra-ui/react";
+import { useSpeechRecognition } from "react-speech-kit";
 
 const Record = () => {
-  return (
-    <div>Record</div>
-  )
-}
+  const [value, setValue] = useState("");
+  const { listen, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      console.log("hello");
+      setValue(result);
+    },
+  });
 
-export default Record
+  return (
+    <Box display="flex" backgroundColor={"red"}>
+      <VStack>
+        <Textarea
+          resize={"vertical"}
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <Button onMouseDown={listen} onMouseUp={stop}>
+          🎤
+        </Button>
+        <Button
+          onClick={() => {
+            setValue("he");
+          }}
+        >
+          Add entry
+        </Button>
+      </VStack>
+    </Box>
+  );
+};
+
+export default Record;
