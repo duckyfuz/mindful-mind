10991c9cec18aa68a4a4cc717953371cdd22b4db
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-10-07 17:02:14 +0800

    Scaffolding

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,39 +6,41 @@ import { Text } from "@chakra-ui/react";
 import { Card, CardBody } from "@chakra-ui/card";
 
 const HomePage = () => {
-  
   return (
     <Box h={"100vh"}>
-      {true && <Text>A note from us</Text>}
+      {true && (
+        <>
+          <Text>A note from us</Text>
+          <Card
+            bg="tomato"
+            w="90vw"
+            h={10}
+            textAlign="left"
+            padding={2}
+            paddingLeft={5}
+          >
+            <Grid
+              templateColumns="repeat(5, 1fr)"
+              gap={6}
+              height="100%"
+              bg="tomato"
+            >
+              <GridItem colSpan={2}>
+                <h2>A Note From Us!</h2>
+              </GridItem>
+              <GridItem colStart={7}>
+                <Button>Test!</Button>
+              </GridItem>
+            </Grid>
+          </Card>
+        </>
+      )}
       <Card>
         <CardBody>
           <Text>Title</Text>
         </CardBody>
       </Card>
       <Text>Past Entries</Text>
-
-      {/* <Card
-        bg="tomato"
-        w="90vw"
-        h={10}
-        textAlign="left"
-        padding={2}
-        paddingLeft={5}
-      >
-        <Grid
-          templateColumns="repeat(5, 1fr)"
-          gap={6}
-          height="100%"
-          bg="tomato"
-        >
-          <GridItem colSpan={2}>
-            <h2>A Note From Us!</h2>
-          </GridItem>
-          <GridItem colStart={7}>
-            <Button>Test!</Button>
-          </GridItem>
-        </Grid>
-      </Card> */}
     </Box>
   );
 };
