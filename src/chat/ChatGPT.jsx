498f08bd86617595685ab8f78a760e75807ebf45
duckyfuz@iMac&cheese <108561447+duckyfuz@users.noreply.github.com>
2023-10-07 13:41:48 +0800

    Add TTS, Firebase

--- src/chat/ChatGPT.jsx
+++ src/chat/ChatGPT.jsx
@@ -1,14 +1,20 @@
-import { Button, Textarea, Stack } from "@chakra-ui/react";
 import { useState } from "react";
-// import axios from "axios";
-
+import axios from "axios";
 import OpenAI from "openai";
 
+import { Button, Textarea, Stack } from "@chakra-ui/react";
+
+import { entry } from "../tmp/detailedEntry";
+
 const ChatGPT = () => {
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState(entry);
+  const [loadingGPT, setLoadingGPT] = useState(false);
+
+  const [response, setResponse] = useState("");
 
   const handleGPT = () => {
     (async () => {
+      setLoadingGPT(true);
       const openai = new OpenAI({
         apiKey: import.meta.env.VITE_OPENAI_KEY,
         dangerouslyAllowBrowser: true,
@@ -19,7 +25,10 @@ const ChatGPT = () => {
         messages: [
           {
             role: "user",
-            content: `Tell me why... Finish it`,
+            content: `Pretend that you are a therapist experienced in the field of mental health.\nOne of your patients wrote the following journal entry:\n
+            ${entry}\n
+            Please give him some advice and avenues where he can seek help. Write it in the form of a monologue from you to him, as if you were speaking to him face to face. Try to be as nuturing as possible, gently guiding him to seeking help.\n
+            At the end, please suggest some activities he can take part in to alleviate his troubles.`,
           },
         ],
         temperature: 0.3,
@@ -29,7 +38,54 @@ const ChatGPT = () => {
         presence_penalty: 0,
       });
       const result = response.choices[0].message.content;
-      console.log(result);
+      setResponse(result);
+      setLoadingGPT(false);
+    })();
+  };
+
+  const TTS = () => {
+    let text = "";
+    if (response.length !== 0) {
+      text = response;
+    } else {
+      text = "Nothing here.";
+    }
+
+    // Create a new SpeechSynthesisUtterance object
+    let utterance = new SpeechSynthesisUtterance();
+
+    // Set the text and voice of the utterance
+    utterance.text = text;
+    utterance.voice = window.speechSynthesis.getVoices()[0];
+
+    // Speak the utterance
+    window.speechSynthesis.speak(utterance);
+  };
+
+  const handleFire = () => {
+    (async () => {
+      try {
+        const firebaseDatabaseUrl =
+          "https://feedback-psa-default-rtdb.asia-southeast1.firebasedatabase.app/";
+        const endpointPath = `v2`;
+
+        const response = await axios.get(
+          `${firebaseDatabaseUrl}${endpointPath}.json`
+        );
+
+        let updatedRows = response.data;
+        updatedRows[new Date()] = inputText;
+
+        console.log(updatedRows);
+
+        await axios.put(
+          `${firebaseDatabaseUrl}${endpointPath}.json`,
+          updatedRows
+        );
+      } catch (error) {
+        console.error("Error adding entry:", error);
+        alert("Please try again later.");
+      }
     })();
   };
 
@@ -42,7 +98,17 @@ const ChatGPT = () => {
           onChange={(e) => setInputText(e.target.value)}
         />
         <Button
-          colorScheme="blue"
+          isLoading={loadingGPT}
+          loadingText="Submitting"
+          onClick={() => {
+            handleFire();
+          }}
+        >
+          Save to Fire
+        </Button>
+        <Button
+          isLoading={loadingGPT}
+          loadingText="Submitting"
           onClick={() => {
             console.log("handling gpt");
             handleGPT();
@@ -50,6 +116,15 @@ const ChatGPT = () => {
         >
           Send to ChatGPT
         </Button>
+        {response.length !== 0 && (
+          <Button
+            onClick={() => {
+              TTS();
+            }}
+          >
+            Play
+          </Button>
+        )}
       </Stack>
     </div>
   );
